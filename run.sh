35f546a95943cab2218d1c41086350b40106d41c
aandito <aandito7@gmail.com>
2022-01-05 11:34:05 +0000

    added run script

---
+++ run.sh
@@ -0,0 +1,2 @@
+nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/database_watcher.py > ./logs/database-watcher.log &
+nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/poller.py > ./logs/poller.log &
