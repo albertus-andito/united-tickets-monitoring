9b1b2623d1a43874f69c60082c234b12ff8be5c2
aandito <aandito7@gmail.com>
2022-01-05 11:43:05 +0000

    modified run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+mkdir -p logs
 nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/database_watcher.py > ./logs/database-watcher.log &
 nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/poller.py > ./logs/poller.log &
