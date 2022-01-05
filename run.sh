67faa8629278ba858c6606282f2076a804067c3b
aandito <aandito7@gmail.com>
2022-01-05 12:50:04 +0000

    modified run script to output stderr

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mkdir -p logs
-nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/database_watcher.py >/dev/null 2>&1 &
-nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/poller.py >/dev/null 2>&1 &
+nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/database_watcher.py >/dev/null 2>> ./logs/database-watcher.log &
+nohup ./united-tickets-monitoring-venv/bin/python ./united-tickets-monitoring/poller.py >/dev/null 2>> ./logs/poller.log &
